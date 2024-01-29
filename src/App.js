21daa2517246dfbb90e06a0390a3194c9d9f4ebc
Branden.Lacour <branden.lacour@equipmentshare.com>
2024-01-29 15:24:02 -0600

    Revert "added filtering and code optimization suggestions"
    This reverts commit 3bc895f63b2ff0b455e92d2922545fc0bc724b1c.

--- src/App.js
+++ src/App.js
@@ -5,35 +5,29 @@ import './App.css';
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchInput, setSearchInput] = useState("");
-  const [productType, setProductType] = useState("");
+  const [searchProduct, setProductInput] = useState("");
 
   useEffect(() => {
     API.getProduct().then((res) => {
       setProducts(res.data);
     });
-  }, []);
+  });
 
-  useEffect(() => {
-    const filtered = products.filter((product) => {
-      const productName = product.name.toLowerCase();
-      const type = product.type.toLowerCase();
-      const input = searchInput.toLowerCase();
-      const selectedProductType = productType.toLowerCase();
+  const cardResults = [];
 
-      const matchesSearch = !searchInput || productName.includes(input);
-      const matchesType = !productType || type === selectedProductType;
-
-      return matchesSearch && matchesType;
-    });
-
-    setFilteredProducts(filtered);
-  }, [searchInput, productType, products]);
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault(); // Prevent form submission
-  };
+  for (let i = 0; i < products.length; i++) {
+    if (searchProduct || searchInput) {
+      if (searchProduct && products[i].type.match(searchProduct)) {
+        cardResults.push(<Card cardResults={products[i]} />);
+      }
+      if (searchInput && products[i].name.match(searchInput)) {
+        cardResults.push(<Card cardResults={products[i]} />);
+      }
+    } else {
+      cardResults.push(<Card cardResults={products[i]} />);
+    }
+  }
 
   return (
     <>
@@ -43,7 +37,7 @@ function App() {
         </div>
       </div>
       <div className="container search">
-        <form onSubmit={handleFormSubmit}>
+        <form>
           <label>
             Search:
             <input
@@ -53,29 +47,19 @@ function App() {
               onChange={(e) => setSearchInput(e.target.value)}
             />
           </label>
-          <label htmlFor="type">Choose a product type:</label>
-          <select
-            onChange={(e) => setProductType(e.target.value)}
-            value={productType}
-          >
-            <option value="ALL">All</option>
-            <option value="RETAIL">Retail</option>
-            <option value="CASH">Cash</option>
-          </select>
         </form>
+        <label for="type">Choose a product type:</label>
+        <select onChange={(e) => setProductInput(e.target.value)}>
+          <option value="RETAIL">Retail</option>
+          <option value="CASH">Cash</option>
+        </select>
       </div>
 
       <div className="container">
         <h1>Results: </h1>
-        <div className="results">
-          {filteredProducts.length > 0
-            ? filteredProducts.map((product, i) => (
-              <Card key={i} cardResults={product} />
-            ))
-            : <p>No results found</p>}
-        </div>
+        <div className="results">{cardResults}</div>
       </div>
-      <div style={{ borderTop: '1px solid #ccc' }} className="footer">
+      <div style={{ borderTop: '1px solid #ccc'}} className="footer">
         {" "}
         <div className="container">
           <p>Built with love ❤️</p>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,43 @@
-import React, { useState } from "react";
-import "./Card.css";
+import React from "react";
+import './Card.css';
 
-function Card({ cardResults: { name, image, type, brandName, price, storeName } }) {
-  const [clicked, setClicked] = useState(false)
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+    };
+  }
 
-  const handleClick = () => {
-    setClicked(!clicked)
+  handleClick = (e) => {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
   };
 
-  return (
-    <div className="card" style={{ borderColor: clicked ? '#333' : '#ccc' }} onClick={handleClick}>
-      {clicked && (
-        <div>
-          <p>Price: {price}</p>
-          <p>Store: {storeName}</p>
-        </div>
-      )}
-      <h1 className="card-title">{name}</h1>
-      <img
-        alt="alt"
-        src={image}
-        style={{ maxWidth: 100, maxHeight: "auto" }}
-      ></img>
-      <p>Type: {type}</p>
-      <p>Brand: {brandName}</p>
-    </div>
-  );
-}
+  render() {
+    const { name, image, type, brandName, price, storeName } =
+      this.props.cardResults;
 
+    return (
+      <div className="card" style={{borderColor: this.state.clicked ? '#333' : '#ccc'}} onClick={this.handleClick}>
+        {this.state.clicked && (
+          <div>
+            <p>Price: {price}</p>
+            <p>{storeName}</p>
+          </div>
+        )}
+        <h1 className="card-title">{name}</h1>
+        <img
+          alt="alt"
+          src={image}
+          style={{ maxWidth: 100, maxHeight: "auto" }}
+        ></img>
+        <p>{type}</p>
+        <p>{brandName}</p>
+      </div>
+    );
+  }
+}
 
 export default Card;
