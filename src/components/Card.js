3bc895f63b2ff0b455e92d2922545fc0bc724b1c
Branden.Lacour <branden.lacour@equipmentshare.com>
2024-01-29 15:22:54 -0600

    added filtering and code optimization suggestions

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,32 @@
-import React from "react";
-import './Card.css';
+import React, { useState } from "react";
+import "./Card.css";
 
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      clicked: false,
-    };
-  }
+function Card({ cardResults: { name, image, type, brandName, price, storeName } }) {
+  const [clicked, setClicked] = useState(false)
 
-  handleClick = (e) => {
-    this.setState({
-      clicked: !this.state.clicked,
-    });
+  const handleClick = () => {
+    setClicked(!clicked)
   };
 
-  render() {
-    const { name, image, type, brandName, price, storeName } =
-      this.props.cardResults;
-
-    return (
-      <div className="card" style={{borderColor: this.state.clicked ? '#333' : '#ccc'}} onClick={this.handleClick}>
-        {this.state.clicked && (
-          <div>
-            <p>Price: {price}</p>
-            <p>{storeName}</p>
-          </div>
-        )}
-        <h1 className="card-title">{name}</h1>
-        <img
-          alt="alt"
-          src={image}
-          style={{ maxWidth: 100, maxHeight: "auto" }}
-        ></img>
-        <p>{type}</p>
-        <p>{brandName}</p>
-      </div>
-    );
-  }
+  return (
+    <div className="card" style={{ borderColor: clicked ? '#333' : '#ccc' }} onClick={handleClick}>
+      {clicked && (
+        <div>
+          <p>Price: {price}</p>
+          <p>Store: {storeName}</p>
+        </div>
+      )}
+      <h1 className="card-title">{name}</h1>
+      <img
+        alt="alt"
+        src={image}
+        style={{ maxWidth: 100, maxHeight: "auto" }}
+      ></img>
+      <p>Type: {type}</p>
+      <p>Brand: {brandName}</p>
+    </div>
+  );
 }
+
 
 export default Card;
